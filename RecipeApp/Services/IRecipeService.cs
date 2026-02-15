505300e9db98c818c41ddc96cf1d5bf9e29b5da8
Adriana-st <s00273868@atu.ie>
2026-02-15 10:06:28 +0000

    added Recipe Service

---
+++ RecipeApp/Services/IRecipeService.cs
@@ -0,0 +1,23 @@
+﻿using RecipeApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeApp.Services
+{
+    /// <summary>
+    /// Interface for recipe operations
+    /// Demonstrates: Interfaces requirement
+    /// </summary>
+    public interface IRecipeService
+    {
+        // Contract: any class implementing this MUST have these methods
+        Task<List<Recipe>> GetRecipesAsync();
+        Task<Recipe> GetRecipeByIdAsync(int id);
+        Task<List<Recipe>> SearchRecipesAsync(string searchTerm);
+        Task<List<Recipe>> FilterByCuisineAsync(string cuisineType);
+        Task<List<Recipe>> FilterByTimeAsync(int maxMinutes);
+    }
+}
